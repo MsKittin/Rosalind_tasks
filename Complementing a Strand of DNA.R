06e2c22e0128a2fff034bd594e52a16ee2c04a9d
Ekaterina Kirillova <59739240+MsKittin@users.noreply.github.com>
2020-01-14 12:50:21 +0300

    Update Complementing a Strand of DNA.R

--- Complementing a Strand of DNA.R
+++ Complementing a Strand of DNA.R
@@ -1,13 +1,9 @@
-#load plyr library
-library(plyr)
-#open the file
-rosalind_revc <- read.table("rosalind_revc.txt")
-#translate our input string into a vector, split it and refer to the inner list of single characters
-dna <- strsplit(as.vector(unlist(rosalind_revc), "any"), split = "") [[1]]
-#reverse string
-reversed_dna <- rev(dna)
-#revalue each nucleotide with it's complement 
-complement <- revalue(reversed_dna, c("A" = "T", "G" = "C", "C" = "G", "T" = "A"))
-#print the result in a format requested by Rosalind
-paste(complement, collapse = "")
+# The task is to write reversed complementary nucleotide sequence of given a DNA string of length at most 1000 bp.
+# Code must return the reversed complement of given DNA string.
 
+library(plyr)
+rosalind_revc <- read.table("rosalind_revc.txt")
+dna <- strsplit(as.vector(unlist(rosalind_revc), "any"), split = "") [[1]]
+reversed_dna <- rev(dna)
+complement <- revalue(reversed_dna, c("A" = "T", "G" = "C", "C" = "G", "T" = "A"))
+paste(complement, collapse = "")
