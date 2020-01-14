62b838b218645b61a73bf66623062d064c96f86e
Ekaterina Kirillova <59739240+MsKittin@users.noreply.github.com>
2020-01-14 12:56:43 +0300

    Update Mendel's First Law.R

--- Mendel's First Law.R
+++ Mendel's First Law.R
@@ -1,3 +1,8 @@
+# Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms:
+# k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+# Return: The probability that two randomly selected mating organisms will produce an individual possessing
+# a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
+
 mendel <- read.table("rosalind_iprb.txt")
 k = mendel[1,1]
 m = mendel[1,2]
