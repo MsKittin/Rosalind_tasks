1b427390bc2006cc8ccddab436dd1f9d094feefb
Ekaterina Kirillova <59739240+MsKittin@users.noreply.github.com>
2020-01-14 12:47:10 +0300

    Update Counting DNA Nucleotides.R

--- Counting DNA Nucleotides.R
+++ Counting DNA Nucleotides.R
@@ -1,3 +1,8 @@
+# The task is to calculate the number of nucleotides 'A', 'C', 'G', and 'T' encountered in the DNA string
+# of length at most 1000 nt. Code must return four integers (separated by spaces) counting the respective 
+# number of times that the symbols 'A', 'C', 'G', and 'T' occur in the DNA string.
+
+
 #open the file
 rosalind_dna <- read.table("rosalind_dna.txt")
 #translate DNA string into a vector
