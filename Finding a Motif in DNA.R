b23b7f5f7ca2eaaf1ee0697af410854f400581a2
Ekaterina Kirillova <59739240+MsKittin@users.noreply.github.com>
2020-02-12 22:05:20 +0300

    Update Finding a Motif in DNA.R

--- Finding a Motif in DNA.R
+++ Finding a Motif in DNA.R
@@ -1,3 +1,6 @@
+# Given: Two DNA strings s and t (each of length at most 1 kbp).
+# Return: All locations of t as a substring of s.
+
 library(stringr)
 dna_string <- read.table("rosalind_subs.txt")
 print(as.integer(gregexpr(paste("(?=",dna_string[2,1],")", sep=""), dna_string[1,1], perl = TRUE)[[1]]))
