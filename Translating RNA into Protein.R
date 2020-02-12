ce809e32b18d2da29b1dfcf15fcc3abfa3307293
Ekaterina Kirillova <59739240+MsKittin@users.noreply.github.com>
2020-02-12 22:01:21 +0300

    Update Translating RNA into Protein.R

--- Translating RNA into Protein.R
+++ Translating RNA into Protein.R
@@ -1,6 +1,8 @@
+#Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+#Return: The protein string encoded by s.
+
 library(Biostrings)
 rna_seq <- read.table("rosalind_prot.txt")
 rna <- RNAString(as.vector(unlist(rna_seq), "any"))
 aminoacid <- Biostrings::translate(rna)
 print(toString(aminoacid))
-
