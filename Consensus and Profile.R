a93e7f5ca3154e87f8cf21aadddc0aac0afa0013
Ekaterina Kirillova <59739240+MsKittin@users.noreply.github.com>
2020-02-16 19:53:50 +0300

    Update Consensus and Profile.R

--- Consensus and Profile.R
+++ Consensus and Profile.R
@@ -1,3 +1,7 @@
+# Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
+# Return: A consensus string and profile matrix for the collection. 
+# (If several possible consensus strings exist, then you may return any one of them.)
+
 library(seqinr)
 fasta <- read.fasta("rosalind_cons.txt")
 sink("consensus.log", type=c("output", "message"))
